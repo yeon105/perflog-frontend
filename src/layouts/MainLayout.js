a5e4a66b23a824c71003273d789b73ace3771df5
Heeyeon An <gmldus4613@naver.com>
2025-11-04 08:36:32 +0900

    style(layout): 메인 레이아웃 스타일 수정

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,20 +1,29 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
     <div>
-      <header style={{ padding: 12, borderBottom: "1px solid #eee" }}>
-        <Link to="/" style={{ fontWeight: 700, fontSize: 18 }}>
-          PerfLog
-        </Link>
-        <nav style={{ marginLeft: 16, display: "inline-block" }}>
-          <Link to="/login" style={{ marginRight: 8 }}>
-            로그인
+      <header style={{ borderBottom: "1px solid #eee" }}>
+        <div
+          className="container"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: 12,
+          }}
+        >
+          <Link to="/" style={{ fontWeight: 700, fontSize: 18 }}>
+            PerfLog
           </Link>
-          <Link to="/signup">회원가입</Link>
-        </nav>
+          <nav style={{ display: "flex", gap: 12 }}>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </nav>
+        </div>
       </header>
-      <main style={{ padding: 16 }}>
+
+      <main className="container" style={{ padding: 16 }}>
         <Outlet />
       </main>
     </div>
